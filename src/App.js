a7001720f668f10f2c66f4dae06982c96f1466ec
Ben Anderson <ben@Bens-MacBook-Pro.local>
2019-10-28 15:24:30 -0500

    johns test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      menuStatus: 'menu-close'
+    }
+  }
+
+  handleClick = () => {
+    if (this.state.menuStatus === 'menu-close') {
+      this.setState({ menuStatus: 'menu-open' })
+    } else {
+      this.setState({ menuStatus: 'menu-close' })
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <nav>
+            <h1 className='logo'>Start Bootstrap</h1>
+            <button className='menu-button' onClick={this.handleClick}>MENU</button>
+            <ul>
+              <li>SERVICES</li>
+              <li>PORTFOLIO</li>
+              <li>ABOUT</li>
+              <li>TEAM</li>
+              <li>CONTACT</li>
+            </ul>
+          </nav>
+          <menu id='menu' className={this.state.menuStatus}>
+            <h1 className='menu-list'>SERVICES</h1>
+            <h1 className='menu-list'>PORTFOLIO</h1>
+            <h1 className='menu-list'>ABOUT</h1>
+            <h1 className='menu-list'>TEAM</h1>
+            <h1 className='menu-list'>CONTACT</h1>
+          </menu>
+        </header>
+      </>
+    );
+  }
 }
 
 export default App;
