a7c873191f49a7bf9650a5ec65c73fc87325df7f
Ben Anderson <ben@Bens-MacBook-Pro.local>
2019-10-28 17:09:07 -0500

    i hate css

--- src/App.js
+++ src/App.js
@@ -22,24 +22,24 @@ class App extends React.Component {
     return (
       <>
         <header>
-          <nav>
-            <h1 className='logo'>Start Bootstrap</h1>
-            <button className='menu-button' onClick={this.handleClick}>MENU</button>
-            <ul>
-              <li>SERVICES</li>
-              <li>PORTFOLIO</li>
-              <li>ABOUT</li>
-              <li>TEAM</li>
-              <li>CONTACT</li>
-            </ul>
-          </nav>
-          <menu id='menu' className={this.state.menuStatus}>
+        <nav>
+          <h1 className='logo'>Start Bootstrap</h1>
+          <button className='menu-button' onClick={this.handleClick}>MENU</button>
+          <ul>
+            <li>SERVICES</li>
+            <li>PORTFOLIO</li>
+            <li>ABOUT</li>
+            <li>TEAM</li>
+            <li>CONTACT</li>
+          </ul>
+        </nav>
+        <menu id='menu' className={this.state.menuStatus}>
             <h1 className='menu-list'>SERVICES</h1>
             <h1 className='menu-list'>PORTFOLIO</h1>
             <h1 className='menu-list'>ABOUT</h1>
             <h1 className='menu-list'>TEAM</h1>
             <h1 className='menu-list'>CONTACT</h1>
-          </menu>
+        </menu>
         </header>
       </>
     );
